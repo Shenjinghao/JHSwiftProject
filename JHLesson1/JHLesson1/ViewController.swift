5058d8df2f2226338331faf7b1e77a79520c1c4b
Shenjinghao <iosdeveloper2013@163.com>
2018-03-16 14:27:14 +0800

    30-1-timer

---
+++ JHLesson1/JHLesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JHLesson1
+//
+//  Created by 作业帮 on 2017/10/27.
+//  Copyright © 2017年 SJH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
